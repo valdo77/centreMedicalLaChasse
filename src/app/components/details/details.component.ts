59adec0debbb21051ddf2c96e211f145eb6a522c
Valdo Negou <valdonegou77@gmail.com>
2020-05-31 21:03:18 +0100

    ajouter du component responsive details

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  services = {};
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient ) { }
 
   ngOnInit(): void {
+    this.getData();
+    setTimeout(() => {
+      document.getElementById('details').scrollIntoView({behavior:'smooth', block: "center"})
+    }, 300);
+  }
+
+
+  getData() {
+    this.activatedRoute.params.subscribe(data => {
+      let id  = data.id;
+
+      this.http.get('assets/data/data.json', {}).subscribe((data:any) => {
+        data.forEach(element => {
+            if(element.id === +id) {
+              this.services = element;
+              return;
+            }
+        });
+      })
+
+    })
   }
 
 }
